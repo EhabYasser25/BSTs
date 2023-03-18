59b59b4032a57f752f4642df156c12be18f83241
Rowaina <rowainaabdelnasser@gmail.com>
2023-03-18 11:09:50 +0200

    Some changes in the trees model

--- src/Service/TreeFactory.java
+++ src/Service/TreeFactory.java
@@ -7,7 +7,7 @@ import Data_Structures.Tree.TreeType;
 
 public class TreeFactory {
     // Tree factory takes the type of the tree and root of the tree and returns a tree.
-
+    // Can be removed if the return type of Tree<T> does not work
     public Tree<String> getTree(TreeType tree) {
         if(tree == TreeType.RB) {
             return new AVLTree<>();
