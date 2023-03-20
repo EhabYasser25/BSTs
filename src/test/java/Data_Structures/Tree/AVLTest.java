23b0f3cdc9a7f4712b46c3ec118e11d7db7df5b1
Maro1123 <96551919+Maro1123@users.noreply.github.com>
2023-03-20 16:35:43 +0200

    Added Test Classes
    added empty tests for rb and avl tree

---
+++ src/test/java/Data_Structures/Tree/AVLTest.java
@@ -0,0 +1,77 @@
+package Data_Structures.Tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AVLTest extends BST {
+
+
+    @Test
+    void test1() {
+    }
+
+    @Test
+    void test2() {
+    }
+
+    @Test
+    void test3(){
+
+    }
+
+    @Test
+    void test4(){
+    }
+
+    @Test
+    void test5() {
+    }
+
+    @Test
+    void test6() {
+    }
+
+    @Test
+    void test7(){
+
+    }
+
+    @Test
+    void test8(){
+    }
+
+    @Test
+    void test9() {
+    }
+
+    @Test
+    void test10() {
+    }
+
+    @Test
+    void test11(){
+
+    }
+
+    @Test
+    void test12(){
+    }
+
+    @Test
+    void test13() {
+    }
+
+    @Test
+    void test14() {
+    }
+
+    @Test
+    void test15(){
+
+    }
+
+    @Test
+    void test16(){
+    }
+}
